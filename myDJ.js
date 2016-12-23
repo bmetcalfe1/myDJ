9d8f3496ed489d9f72aa0a7825b0a5c0a4ab5b84
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-12-23 14:53:36 -0500

    pauses on scratch plays after scratch

--- myDJ.js
+++ myDJ.js
@@ -5,12 +5,18 @@ function playSong() {
 function pauseSong() { 
     theSong.pause(); 
 }
+
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (!audio) return; // stop the function from running all together
-  //pauseSong();
+  pauseSong();
   audio.currentTime = 0; // rewind to the start
   audio.play();
+  audio.addEventListener("ended", function() {
+    if (theSong.currentTime != 0) {
+      playSong();
+    }
+  });
 }
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return; // skip it if it's not a transform
